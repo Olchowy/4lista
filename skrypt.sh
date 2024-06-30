9be3b07ed741c95207a36d1591c57c5d2387b939
Olchowy <pawel.olchowy@o2.pl>
2024-06-30 20:12:25 +0200

    dodanie instrukcji )

--- skrypt.sh
+++ skrypt.sh
@@ -7,6 +7,12 @@ elif [ "$1" == "--logs" ]; then
     for ((i=1; i<=num_files; i++)); do
         echo -e "Plik numer: $i\nNazwa skryptu: $0\nData: $(date)" > log$i.txt
     done
+elif [ "$1" == "--help" ]; then
+    echo "Dostępne opcje:"
+    echo "--date        Wyświetla dzisiejszą datę"
+    echo "--logs [liczba] Tworzy pliki log_x.txt z informacjami"
+    echo "--help        Wyświetla pomoc"
 else
-    echo "Użycie: $0 --date | --logs [liczba]"
+    echo "Użycie: $0 --date | --logs [liczba] | --help"
 fi
+
