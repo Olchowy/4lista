7cb84529e8cbcea51e603c575b09f5de1763d078
Olchowy <pawel.olchowy@o2.pl>
2024-06-30 20:06:04 +0200

    dodanie --logi

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,11 @@
 
 if [ "$1" == "--date" ]; then
     date
+elif [ "$1" == "--logs" ]; then
+    numfiles=${2:-100}
+    for ((i=1; i<=num_files; i++)); do
+        echo -e "Plik numer: $i\nNazwa skryptu: $0\nData: $(date)" > log$i.txt
+    done
 else
-    echo "Użycie: $0 --date"
+    echo "Użycie: $0 --date | --logs [liczba]"
 fi
